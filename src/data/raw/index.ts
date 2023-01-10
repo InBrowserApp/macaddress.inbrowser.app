b5684a4e580e5664c3f8e7004b7fbb6584007241
Seedgou <i@zczc.cz>
2023-01-10 10:54:02 +0000

    add index.ts export for data

---
+++ src/data/raw/index.ts
@@ -0,0 +1,24 @@
+export {
+  getEntryFromAssignment as getCIDEntryFromAssignment,
+  type CIDEntry,
+} from "./cid";
+
+export {
+  getEntryFromAssignment as getIABEntryFromAssignment,
+  type IABEntry,
+} from "./iab";
+
+export {
+  getEntryFromAssignment as getMALEntryFromAssignment,
+  type MALEntry,
+} from "./ma-l";
+
+export {
+  getEntryFromAssignment as getMAMEntryFromAssignment,
+  type MAMEntry,
+} from "./ma-m";
+
+export {
+  getEntryFromAssignment as getMASEntryFromAssignment,
+  type MASEntry,
+} from "./ma-s";
