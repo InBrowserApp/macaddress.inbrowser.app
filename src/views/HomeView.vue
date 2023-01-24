6de54cd319d4767913d40f16cbf18b8a3b9cff40
Seedgou <i@zczc.cz>
2023-01-24 12:11:24 +0000

    add heading for home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <main>
+    <n-h1 prefix="bar" align-text>Search</n-h1>
     <EntrySearch />
   </main>
 </template>
 
 <script setup lang="ts">
+import { NH1 } from "naive-ui";
 import EntrySearch from "@/components/search/auto-complete/EntrySearch.vue";
 </script>
