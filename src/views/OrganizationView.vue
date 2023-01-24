2a422b8a57b2acf089c5d43079164fb8245f77b1
Seedgou <i@zczc.cz>
2023-01-24 12:03:59 +0000

    add organization view

---
+++ src/views/OrganizationView.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <n-h1 prefix="bar" align-text>
+      {{ organization }}
+    </n-h1>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { NH1 } from "naive-ui";
+import { useRouteParams } from "@vueuse/router";
+
+const organization = useRouteParams<string>("organization");
+</script>
