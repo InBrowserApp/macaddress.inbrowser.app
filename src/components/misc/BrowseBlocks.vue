5f98ddd1195f48a6c2106b85fb1eca136f2c27d4
Seedgou <i@zczc.cz>
2023-01-25 14:21:27 +0000

    add browse by blocks

---
+++ src/components/misc/BrowseBlocks.vue
@@ -0,0 +1,29 @@
+<template>
+  <n-dropdown trigger="hover" :options="options" @select="handleSelect">
+    <n-button text size="large">
+      <template #icon>
+        <n-icon :component="BlockOutlined" />
+      </template>
+      Browse By Blocks
+    </n-button>
+  </n-dropdown>
+</template>
+
+<script lang="ts" setup>
+import { NButton, NIcon, NDropdown } from "naive-ui";
+import BlockOutlined from "@vicons/antd/BlockOutlined";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const blocks = ["MA-L", "MA-M", "MA-S", "CID", "IAB"];
+
+const options = blocks.map((block) => ({
+  label: block,
+  key: block,
+}));
+
+const handleSelect = (key: string) => {
+  const url = `/${key}`;
+  router.push(url);
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,9 @@
       <n-grid-item>
         <BrowseOrganizations />
       </n-grid-item>
+      <n-grid-item>
+        <BrowseBlocks />
+      </n-grid-item>
     </n-grid>
   </main>
 </template>
@@ -15,4 +18,5 @@
 import { NH2, NGrid, NGridItem } from "naive-ui";
 import EntrySearch from "@/components/search/auto-complete/EntrySearch.vue";
 import BrowseOrganizations from "@/components/misc/BrowseOrganizations.vue";
+import BrowseBlocks from "@/components/misc/BrowseBlocks.vue";
 </script>
