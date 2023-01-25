d61cf40d4faff34b84d6531a36808717887290ed
Seedgou <i@zczc.cz>
2023-01-25 08:01:13 +0000

    add organization list view

--- src/views/OrganizationView.vue
+++ src/views/OrganizationView.vue
@@ -4,16 +4,35 @@
       {{ organization }}
     </n-h1>
     <n-p>
-      {{ assignments }}
+      <n-list>
+        <n-list-item
+          v-for="assignment in assignments"
+          :key="assignment.Assignment"
+        >
+          <template #suffix>
+            <RegistryTag
+              type="success"
+              size="small"
+              :registry="assignment.Registry"
+              round
+            />
+          </template>
+          <RouterLink :to="`/${assignment.Registry}/${assignment.Assignment}`">
+            <AssignmentDisplay :assignment="assignment.Assignment" />
+          </RouterLink>
+        </n-list-item>
+      </n-list>
     </n-p>
   </main>
 </template>
 
 <script setup lang="ts">
-import { NH1 } from "naive-ui";
+import { NH1, NList, NP, NListItem } from "naive-ui";
 import { useRouteParams } from "@vueuse/router";
 import { computedAsync } from "@vueuse/core";
 import { searchOrganization } from "@/data/blocks";
+import AssignmentDisplay from "@/components/display/AssignmentDisplay.vue";
+import RegistryTag from "@/components/display/RegistryTag.vue";
 
 const organization = useRouteParams<string>("organization");
 const assignments = computedAsync(async () => {
