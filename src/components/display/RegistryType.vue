d46331e7059529440f9b974adc07109a6111fc0e
Seedgou <i@zczc.cz>
2023-01-25 16:50:38 +0000

    update entry display registry

--- src/components/display/RegistryType.vue
+++ src/components/display/RegistryType.vue
@@ -1,13 +1,16 @@
 <template>
   <n-text>
-    {{ registry }}
+    {{ `${getBlockFullName(registry)} (${registry})` }}
   </n-text>
 </template>
 
 <script setup lang="ts">
 import { NText } from "naive-ui";
+import { getBlockFullName } from "@/utils/block";
+
+import type { Block } from "@/data/blocks";
 
 defineProps<{
-  registry: string;
+  registry: Block;
 }>();
 </script>
